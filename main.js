4976af02175c2151316375bbc5aa6b2c1da84a91
Brekden <sbrekden@gmail.com>
2025-03-28 10:37:59 -0500

    Assign currentplayer values to p1 & p2.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//gives framework for gameboard
 function createGameBoard(rows, cols) {
     const board = [];
     for (let i= 0; i < rows; i++) {
@@ -9,6 +10,7 @@ function createGameBoard(rows, cols) {
     return board;  
 }
 
+//creates the gameboard using previous function
 const gameBoard = createGameBoard(3, 3);
 console.log(gameBoard)
 
@@ -16,22 +18,29 @@ class Player {
     constructor(name, marker) {
         this.name = name;
         this.marker = marker;
-        function sayName(name) {
-            console.log(name)
-        }
     }
 }
-const p1 = new Player('Player 1', 'x')
-const p2 = new Player('Player 2', 'o')
+//create players 1 and 2 (p1,p2) [change names back to Player 1 and Player 2 when finished]
+const p1 = new Player('Max', 'x')
+const p2 = new Player('Ruby', 'o')
 
-//alternating turns
+//creates alternating turns
 let currentPlayer = 1;
-
 function nextTurn() {
     currentPlayer = 3 - currentPlayer;
     console.log(`current player: Player ${currentPlayer}`);
 }
-console.log(`current player: Player ${currentPlayer}`);
+console.log(`current player: Player ${currentPlayer}`); //for removal at end
+
+//assigns the currentPlayer value to the created players (p1,p2)
+if (currentPlayer=1) {
+    currentPlayer= p1
+} else {
+    currentPlayer= p2
+}
+
+
+
 
 // determine outcome 
 
